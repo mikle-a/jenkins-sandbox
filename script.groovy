c60a996e00ee02603a9cdb9f24f4479515478db6
m.ananchenko <m.ananchenko@semrush.com>
2022-02-07 00:12:10 +0300

    publish docker image

--- script.groovy
+++ script.groovy
@@ -17,14 +17,24 @@ withPod {
         checkout scm
 
         container('docker') {
+
             stage('Build') {
                 sh("docker build -t ${service} .")
             }
+
             stage('Test') {
                 try {
                     sh("docker run -v `pwd`:/workspace --rm ${service} python setup.py test")
                 } finally {
                     step([$class: 'JUnitResultArchiver', testResults: 'results.xml'])
+                }
+            }
+
+            def tagToDeploy = "mikle-a/${service}"
+            stage('Publish') {
+                withDockerRegistry(registry: [credentialsId: 'dockerhub']) {
+                    sh("docker tag ${service} ${tagToDeploy}")
+                    sh("docker push ${tagToDeploy}")
                 }
             }
         }
